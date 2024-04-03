3f01d261b421501d4934e3ebb742c37d632b2c8c
Mars1836 <hauhpll123@gmail.com>
2024-04-03 19:28:48 +0700

    add user api

--- src/v1/routes/index.router.js
+++ src/v1/routes/index.router.js
@@ -1,11 +1,23 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+let users = [
+  { name: "John", age: 25, gender: "Male" },
+  { name: "Alice", age: 30, gender: "Female" },
+  { name: "Bob", age: 35, gender: "Male" },
+];
 
-router.get('/checkstatus', (req, res, next) => {
-    res.status(200).json({
-        status: 'success',
-        message: 'api ok'
-    })
-})
+router.get("/checkstatus", (req, res, next) => {
+  res.status(200).json({
+    status: "success",
+    message: "api ok",
+  });
+});
+router.get("/api/user", (req, res, next) => {
+  res.status(200).json({
+    status: "success",
+    message: "api ok",
+    matadata: users,
+  });
+});
 
 module.exports = router;
